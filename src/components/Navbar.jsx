54b042405b4a85a08472c07eaa4a584a39523640
Michael Gerlach <mgerlach0130@gmail.com>
2024-03-23 14:16:26 -0500

    fixed media queries

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,21 +3,19 @@ import { Link } from 'react-router-dom';
 export default function Nav() {
     return (
         <nav className="navbar">
-            <div className="navbar-container">
-                <div className='navlinks'>
-                    <button>
-                        <Link to="/">Home</Link>
-                    </button>
-                    <button>
-                        <Link to="/portfolio">Portfolio</Link>
-                    </button>
-                    <button>
-                        <Link to="/contact">Contact</Link>
-                    </button>
-                    <button>
-                        <Link to="/resume">Resume</Link>
-                    </button>
-                </div>
+            <div className='navlinks'>
+                <button>
+                    <Link to="/">Home</Link>
+                </button>
+                <button>
+                    <Link to="/portfolio">Portfolio</Link>
+                </button>
+                <button>
+                    <Link to="/contact">Contact</Link>
+                </button>
+                <button>
+                    <Link to="/resume">Resume</Link>
+                </button>
             </div>
         </nav>
     );
