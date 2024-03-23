e5ee6d784242b5e4fc3ebbd3c4579333b85b9ba0
Michael Gerlach <mgerlach0130@gmail.com>
2024-03-23 15:05:45 -0500

    error fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,16 +4,16 @@ export default function Nav() {
     return (
         <nav className="navbar">
             <div className='navlinks'>
-                <button>
+                <button style="-webkit-appearance:none;">
                     <Link to="/">Home</Link>
                 </button>
-                <button>
+                <button style="-webkit-appearance:none;">
                     <Link to="/portfolio">Portfolio</Link>
                 </button>
-                <button>
+                <button style="-webkit-appearance:none;">
                     <Link to="/contact">Contact</Link>
                 </button>
-                <button>
+                <button style="-webkit-appearance:none;">
                     <Link to="/resume">Resume</Link>
                 </button>
             </div>
